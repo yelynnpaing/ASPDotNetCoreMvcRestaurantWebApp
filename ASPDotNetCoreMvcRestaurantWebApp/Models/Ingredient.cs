1ed04cb7732a3d489dfa233040af6e2ce0b095f3
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-02-10 22:53:20 +0630

    Add - update  model and migration

--- ASPDotNetCoreMvcRestaurantWebApp/Models/Ingredient.cs
+++ ASPDotNetCoreMvcRestaurantWebApp/Models/Ingredient.cs
@@ -1,9 +1,12 @@
-﻿namespace ASPDotNetCoreMvcRestaurantWebApp.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ASPDotNetCoreMvcRestaurantWebApp.Models
 {
     public class Ingredient
     {
         public int IngredientId { get; set; }
         public string Name { get; set; }
+        [ValidateNever]
         public ICollection<ProductIngredient> ProductIngredients { get; set; }
     }
 }
--- ASPDotNetCoreMvcRestaurantWebApp/Models/Product.cs
+++ ASPDotNetCoreMvcRestaurantWebApp/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASPDotNetCoreMvcRestaurantWebApp.Models
 {
@@ -9,9 +11,15 @@ namespace ASPDotNetCoreMvcRestaurantWebApp.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
-        public int CategoryId { get; set; } 
+        public int CategoryId { get; set; }
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+        public string ImageUrl { get; set; } = "https://via.placeholder.com/150";
+        [ValidateNever]
         public Category? Category { get; set; }
+        [ValidateNever]
         public ICollection<OrderItem>? OrderItems { get; set; }
+        [ValidateNever]
         public ICollection<ProductIngredient>? ProductIngredients { get; set; }
     }
 }
