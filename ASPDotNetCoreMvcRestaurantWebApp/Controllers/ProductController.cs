efc1f4126209f6b36bd0d777f3b82053f36959d2
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-02-19 22:27:07 +0630

    Add - Product Read

---
+++ ASPDotNetCoreMvcRestaurantWebApp/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using ASPDotNetCoreMvcRestaurantWebApp.Data;
+using ASPDotNetCoreMvcRestaurantWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNetCoreMvcRestaurantWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Repository<Product> products;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            products = new Repository<Product>(context);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await products.GetAllAsync());
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            if(id == 0)
+            {
+                ViewBag.Title = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                ViewBag.Title = "Edit";
+                return View();
+            }
+        }
+    }
+}
