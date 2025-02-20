cb729c104d62060b726f03866389d98ff7ebdbd0
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-02-20 23:43:12 +0630

    Add - Create product

--- ASPDotNetCoreMvcRestaurantWebApp/Controllers/ProductController.cs
+++ ASPDotNetCoreMvcRestaurantWebApp/Controllers/ProductController.cs
@@ -7,10 +7,17 @@ namespace ASPDotNetCoreMvcRestaurantWebApp.Controllers
     public class ProductController : Controller
     {
         private Repository<Product> products;
+        private Repository<Ingredient> ingredients;
+        private Repository<Category> categories;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(ApplicationDbContext context)
+
+        public ProductController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             products = new Repository<Product>(context);
+            ingredients = new Repository<Ingredient>(context);
+            categories = new Repository<Category>(context);
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public async Task<IActionResult> Index()
@@ -22,15 +29,58 @@ namespace ASPDotNetCoreMvcRestaurantWebApp.Controllers
         [HttpGet]
         public async Task<IActionResult> AddEdit(int id)
         {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            ViewBag.Categories = await categories.GetAllAsync();
+
             if(id == 0)
             {
-                ViewBag.Title = "Add";
+                ViewBag.Operation = "Add";
                 return View(new Product());
             }
             else
             {
-                ViewBag.Title = "Edit";
+                ViewBag.Operation = "Edit";
                 return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEdit(Product product, int[] ingredientsId, int catId)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.ImageFile != null)
+                {
+                    string uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + product.ImageFile.FileName;
+                    string filePath = Path.Combine(uploadFolder, uniqueFileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await product.ImageFile.CopyToAsync(fileStream);
+                    }
+                    product.ImageUrl = uniqueFileName;
+                }
+
+                if (product.ProductId == 0)
+                {
+                    ViewBag.Ingredients = await ingredients.GetAllAsync();
+                    ViewBag.Categories = await categories.GetAllAsync();
+                    product.CategoryId = catId;
+                    foreach(int id in ingredientsId)
+                    {
+                        product.ProductIngredients?.Add(new ProductIngredient { IngredientId = id, ProductId = product.ProductId });
+                    }
+                    await products.AddAsync(product);
+                    return RedirectToAction("Index", "Product");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Product");
+                }
+            }
+            else
+            {
+                return View(product);
             }
         }
     }
